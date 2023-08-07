37a866418725cc1a8eb605fd2a47b2313a32b815
Pomojkas <knyazkov01@bk.ru>
2023-08-07 18:57:08 +0800

    Initial commit

---
+++ Good snake on WPF/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Good_snake_on_WPF
+{
+    public class GameState
+    {
+        public int Rows { get; }
+        public int Columns { get; }
+        public GridValue[,] Grid { get; }
+        public Direction Direction { get; private set; }
+        public int Score { get; private set; }
+        public bool GaneOver { get; private set; }
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>();
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int columns) 
+        {
+            Rows = rows; 
+            Columns = columns;
+            Grid = new GridValue[rows, columns];
+            Direction = Direction.Right;
+        }
+
+        private void AddSnake()
+        {
+
+        }
+    }
+}
---
+++ Good snake on WPF/GridValue.cs
@@ -0,0 +1,10 @@
+﻿namespace Good_snake_on_WPF
+{
+    public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside
+    }
+}
